71cd7dc36e317bbc47f7302a0d99e811ea7e26b7
shifttag <ehdgus0837@naver.com>
2024-10-28 11:48:28 +0900

    20241028

---
+++ board_front/src/react-study/기초.ts
@@ -0,0 +1,26 @@
+export const tmp = "";
+/* 
+# <<< 단축키. ts >>>
+
+- 새 터미널 열기: Ctrl + Shift + `
+- 터미널 열기: Ctrl + `
+- 줄 복사: Alt + Shift + 방향키(위/아래)
+- 줄 이동: Alt + 방향키(위/아래)
+
+- 들여쓰기: Tab
+- 내어쓰기: Shift + Tab
+
+- 사이드바 토글: Ctrl + B
+
+- 검색: Ctrl + F
+
+- 여러줄 선택: Ctrl + Alt + 방향키(위/아래)
+
+? 리액트 함수형 컴포넌트 생성 단축키 
+: rfc / rfce
+
+? 리액트 실행 명령어
+: npm run start
+
+
+*/
