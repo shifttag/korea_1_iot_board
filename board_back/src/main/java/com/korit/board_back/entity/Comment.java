fff03c68016c8565f20efa1fc9ea34e988b15ff2
shifttag <ehdgus0837@naver.com>
2024-11-07 16:46:54 +0900

    20241107

---
+++ board_back/src/main/java/com/korit/board_back/entity/Comment.java
@@ -0,0 +1,30 @@
+package com.korit.board_back.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Comments")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "article_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Article article;
+
+    @Column(nullable = false, name = "commenter_id")
+    private Long commenterId;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+
+    private String content;
+}
